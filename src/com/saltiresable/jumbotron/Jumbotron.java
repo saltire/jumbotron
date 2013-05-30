d678141508f4f65c1ac66ca90f40bd377f5e71e7
saltire sable <saltiresable@gmail.com>
2013-05-29 23:03:06 -0400

    Added some error handling for a disconnected serial port
    Should now fail silently when the serial connection to the Arduino is
    down, and also attempt to connect to it when attempting to send data.

--- src/com/saltiresable/jumbotron/Jumbotron.java
+++ src/com/saltiresable/jumbotron/Jumbotron.java
@@ -23,13 +23,11 @@ public final class Jumbotron extends JavaPlugin {
 	enum Dir { NORTH, EAST, SOUTH, WEST }
 	Dir dir = Dir.EAST;
 	
-	byte[][] bytes = new byte[512][5];
-	int sent = 0;
+	byte[][] bytes = new byte[w * h][5];
+	int to_send = w * h;
 	
 	@Override
 	public void onEnable() {
-		getLogger().info("Enabled Jumbotron");
-		
 		World world = getServer().getWorld(worldname);
 		
 		int bx = x, by = y, bz = z;
@@ -51,7 +49,6 @@ public final class Jumbotron extends JavaPlugin {
 			for (int v = 0; v < h; v++) {
 				by = y - v;
 				int[] color = getBlockColor(world.getBlockAt(bx, by, bz));
-				getLogger().info("Checking color at "+bx+","+by+","+bz);
 				bytes[u * h + v] = new byte[] {
 					(byte) color[0], (byte) color[1], (byte) color[2],
 					(byte) u, (byte) v
@@ -59,9 +56,10 @@ public final class Jumbotron extends JavaPlugin {
 			}
 		}
 		
-		initPixel();
+		sendNextPixel();
 		
 		getServer().getPluginManager().registerEvents(new BlockListener(this), this);
+		getLogger().info("Enabled Jumbotron");
 	}
 	
 	public boolean coordsInArea(int bx, int by, int bz) {
@@ -111,44 +109,44 @@ public final class Jumbotron extends JavaPlugin {
 	
 	@Override
 	public void onDisable() {
-		try {
-			arduino.serialPort.closePort();
-		} catch (SerialPortException e) {
-			getLogger().severe(e.getMessage());
-		}
+		arduino.disable();
 		getLogger().info("Disabled Jumbotron");
 	}
 	
-	private void initPixel() {
-		if (sent < 512) {
-			byte[] p = bytes[sent];
-			sendPixel(p);
-			sent++;
-		}
+	public boolean sendPixel(byte[] p) {
+		//getLogger().info("Sending pixel:"+p[3]+","+p[4]+": "+p[0]+","+p[1]+","+p[2]);
+		return arduino.sendBytes(p);
 	}
 	
-	public void sendPixel(byte[] p) {
-		try {
-			arduino.serialPort.writeBytes(p);
-			//getLogger().info("Sent pixel:"+p[3]+","+p[4]+": "+p[0]+","+p[1]+","+p[2]);
-		} catch (SerialPortException e) {
-			getLogger().severe(e.getMessage());
+	private void sendNextPixel() {
+		if (to_send > 0) {
+			byte[] p = bytes[w * h - to_send];
+			if (sendPixel(p)) {
+				to_send--;
+			}
 		}
 	}
 	
 	private class ArduinoJSSC implements SerialPortEventListener {
 		
 		SerialPort serialPort;
+		boolean open = false;
 		
 		public ArduinoJSSC(String portName) {
+			serialPort = new SerialPort(portName);
+			open = openPort();
+		}
+		
+		private boolean openPort() {
 			try {
-				serialPort = new SerialPort(portName);
 				serialPort.openPort();
 				serialPort.setParams(9600, 8, 1, 0);
 				serialPort.setEventsMask(SerialPort.MASK_RXCHAR);
 				serialPort.addEventListener(this);
+				return true;
 			} catch (SerialPortException e) {
 				getLogger().severe(e.getMessage());
+				return false;
 			}
 		}
 		
@@ -159,7 +157,7 @@ public final class Jumbotron extends JavaPlugin {
 					try {
 						serialPort.readHexString(3);
 						//getLogger().info(serialPort.readHexString(3));
-			        	initPixel();
+			        	sendNextPixel();
 			        	
 			        	//String string = serialPort.readString();
 			            //getLogger().info("Got string: " + string);
@@ -168,6 +166,27 @@ public final class Jumbotron extends JavaPlugin {
 			        }
 		        }
 		    }
+		}
+		
+		public boolean sendBytes(byte[] bytes) {
+			if (!open && !openPort()) {
+				return false;
+			}
+			try {
+				serialPort.writeBytes(bytes);
+				return true;
+			} catch (SerialPortException e) {
+				getLogger().severe(e.getMessage());
+				return false;
+			}
+		}
+		
+		public void disable() {
+			try {
+				serialPort.closePort();
+			} catch (SerialPortException e) {
+				getLogger().severe(e.getMessage());
+			}
 		}
 	}
 }
